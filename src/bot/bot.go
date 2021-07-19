171542107166e40ef08d8012a9204822b1c00f80
Ryan S <wuuug@telegram>
2021-07-19 14:19:28 -0700

    Update documentation for /findtagtypos

--- src/bot/bot.go
+++ src/bot/bot.go
@@ -150,25 +150,30 @@ indextags. This operation is invoked by <code>/syncposts</code>, which passes <c
 janitor.indextagaliases. <code>/indextagaliases</code>
 indextagaliases. This command syncs tag aliases between ` + api.ApiName + ` and the local alias database. Because of how aliases are listed on ` + api.ApiName + `, an incremental sync is not possible, and a full sync is always performed. This command takes no options. It is invoked by <code>/syncposts</code> if <code>--aliases</code> is specified.
 janitor.findtagtypos. <code>/findtagtypos</code>
-findtagtypos. This command searches for likely typos of a tag, as determined by their edit distance to other tags. The way you should use this command is broadly at first, listing all typos, and then more and more specifically as you investigate each possible option on the site, adding selection options until you have a comprehensive, accurate listing of typos, then apply them to the site by issuing the command again with <code>--fix</code> and using <code>--save</code> to store them for auto-fixing in the future.
+findtagtypos. This command searches for likely typos of a tag, as determined by their edit distance to other tags. The way you should use this command is broadly at first, listing all typos, and then more and more specifically as you investigate each possible option on the site, adding selection options until you have a comprehensive, accurate listing of typos, then apply them to the site by issuing the command again with <code>--fix</code> and using <code>--include</code> or <code>--autofix</code> to register them for future auto-fixes.
 findtagtypos. <i>Listing</i> options:
-findtagtypos. <code> [other args] TAG  -</code> find typos of <code>TAG</code> (required)
-findtagtypos. <code> --all,         -a -</code> include known or ignored possible typos
-findtagtypos. <code> --all-posts,   -p -</code> include deleted posts
-findtagtypos. <code> --show-short,  -s -</code> include tags less than 3 characters long
-findtagtypos. <code> --show-zero,   -z -</code> show tags with zero posts
-findtagtypos. <code> --only-general,-g -</code> hide specialty tags (<code>--include</code> overrides)
-findtagtypos. <code> --threshhold,-t N -</code> show tags with edit distance <code>N</code> or less
+findtagtypos. <code> --no-auto,     -x -</code> do not automatically treat <code>START_TAG</code> alias
+findtagtypos. <code> --list-wild,   -w -</code> show unconfirmed typos (default)
+findtagtypos. <code> --list-yes,    -y -</code> show confirmed typos
+findtagtypos. <code> --list-no,     -n -</code> show confirmed non-typos
+findtagtypos. <code> --list,        -l -</code> same as -y -n
+findtagtypos. <code> --show-blits,  -b -</code> include typos which are blits
+findtagtypos. <code> --show-zero,   -z -</code> include typos with no tagged posts
+findtagtypos. <code> --general-only,-g -</code> include typos which are non-general tags
+findtagtypos. <code> --threshold, -t N -</code> show tags with edit distance <code>N</code> or less
 findtagtypos. <i>Selection</i> options:
-findtagtypos. <code> --exclude,  -e E -</code> mark <code>E</code> as not a typo
-findtagtypos. <code> --distinct, -d E -</code> excludes all tags closer to <code>E</code> than <code>TAG</code>
-findtagtypos. <code> --include,  -i I -</code> mark <code>I</code> as a typo
-findtagtypos. <i>Editing</i> options:
-findtagtypos. Saving changes will store <code>--exclude</code> tags for ignore behavior, and <code>--include</code> tags with prompt-to-fix behavior by default.
-findtagtypos. <code> --save,    -S -</code> store changes for corrective action
-findtagtypos. <code> --autofix, -X -</code> tags are marked for autofix instead of prompt
-findtagtypos. <code> --fix,     -x -</code> push changes to ` + api.ApiName + ` now
-findtagtypos. <code> --reason,-r R -</code> use edit reason <code>R</code> instead of "likely typo"
+findtagtypos. <code> [args] START_TAG -</code> find typos of <code>TAG</code> (required)
+findtagtypos. <code> --select,   -s T -</code> select a specific, arbitrary tag <code>T</code>
+findtagtypos. <code> --skip,     -k T -</code> deselect a specific, arbitrary tag <code>T</code>
+findtagtypos. <code> --alias,    -a T -</code> also search for typos near tag <code>T</code>
+findtagtypos. <code> --distinct, -d T -</code> treat <code>T</code> as distinct, and ignore nearby typos
+findtagtypos. <i>Submission</i> options:
+findtagtypos. <code> --delete,  -D   -</code> delete these typo records, if they exist
+findtagtypos. <code> --exclude, -E   -</code> register the selected tags as non-typos
+findtagtypos. <code> --include, -I   -</code> register the selected tags as typos
+findtagtypos. <code> --autofix, -A   -</code> automatically fix the selected typos
+findtagtypos. <code> --fix,     -F   -</code> fix the selected typos now
+findtagtypos. <code> --reason,  -r R -</code> include reason <code>R</code> when performing edits
 janitor.recounttags. <code>/recounttags</code>
 recounttags. This command recounts the cached tag counts, providing an accurate count (the site itself becomes desynced sometimes and its counts are not always accurate). It does so for both visible and deleted posts. It takes no arguments. It is invoked by <code>/syncposts</code> if the <code>--recount</code> option is specified.
 janitor.resyncdeleted. <code>/resyncdeleted</code>
