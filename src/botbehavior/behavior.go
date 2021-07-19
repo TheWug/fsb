15d5bdb2d2827f416a1dae4c9374bd58f44dff86
Ryan S <wuuug@telegram>
2021-07-19 14:22:40 -0700

    Capture a forgotten error

--- src/botbehavior/behavior.go
+++ src/botbehavior/behavior.go
@@ -117,7 +117,7 @@ func (this *Behavior) StartMaintenanceAsync(bot *gogram.TelegramBot) (chan bool)
 			}()
 
 			go func() {
-				tagindex.SyncPostsInternal(this.MySettings.SearchUser, this.MySettings.SearchAPIKey, settings, extra_expensive, extra_expensive, nil, update_chan)
+				err = tagindex.SyncPostsInternal(this.MySettings.SearchUser, this.MySettings.SearchAPIKey, settings, extra_expensive, extra_expensive, nil, update_chan)
 				close(update_chan)
 				wg.Done()
 				if err != nil { bot.ErrorLog.Println("SyncPostsInternal in maintenance routine:", err.Error()) }
