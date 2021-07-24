365557fc93e8a8a66ea93bdf6757bba83f0dee38
Ryan S <wuuug@telegram>
2021-07-24 14:18:13 -0700

    Purge UpdaterSettings

--- src/storage/webm.go
+++ src/storage/webm.go
@@ -7,11 +7,7 @@ import (
 	"strings"
 )
 
-func FindCachedMp4ForWebm(md5 string, settings UpdaterSettings) (*tgtypes.FileID, error) {
-	mine, tx := settings.Transaction.PopulateIfEmpty(Db_pool)
-	defer settings.Transaction.Finalize(mine)
-	if settings.Transaction.err != nil { return nil, settings.Transaction.err }
-
+func FindCachedMp4ForWebm(tx *sql.Tx, md5 string) (*tgtypes.FileID, error) {
 	query := "SELECT telegram_id FROM webms_converted_for_telegram WHERE md5 = $1"
 
 	row := tx.QueryRow(query, strings.ToLower(md5))
@@ -25,15 +21,10 @@ func FindCachedMp4ForWebm(md5 string, settings UpdaterSettings) (*tgtypes.FileID
 	return &out, nil
 }
 
-func SaveCachedMp4ForWebm(md5 string, id tgtypes.FileID, settings UpdaterSettings) error {
-	mine, tx := settings.Transaction.PopulateIfEmpty(Db_pool)
-	defer settings.Transaction.Finalize(mine)
-	if settings.Transaction.err != nil { return settings.Transaction.err }
-
+func SaveCachedMp4ForWebm(tx *sql.Tx, md5 string, id tgtypes.FileID) error {
 	query := "INSERT INTO webms_converted_for_telegram (md5, telegram_id) VALUES ($1, $2) ON CONFLICT (md5) DO UPDATE SET telegram_id = EXCLUDED.telegram_id"
 
 	_, err := tx.Exec(query, strings.ToLower(md5), id)
 
-	settings.Transaction.commit = mine && (err == nil)
 	return err
 }
