7bb6fc124a93ef8d1b1a8757db0dffc9da862d79
Ryan S <wuuug@telegram>
2021-07-24 14:19:58 -0700

    Purge UpdaterSettings

--- src/storage/dialog.go
+++ src/storage/dialog.go
@@ -4,6 +4,7 @@ import (
 	tgtypes "github.com/thewug/gogram/data"
 
 	"time"
+	"database/sql"
 )
 
 type DialogPost struct {
@@ -14,44 +15,26 @@ type DialogPost struct {
 	DialogData []byte
 }
 
-func WriteDialogPost(settings UpdaterSettings, dialog_id tgtypes.DialogID, msg_id tgtypes.MsgID, chat_id tgtypes.ChatID, json string, msg_ts time.Time) error {
-	mine, tx := settings.Transaction.PopulateIfEmpty(Db_pool)
-	defer settings.Transaction.Finalize(mine)
-	if settings.Transaction.err != nil { return settings.Transaction.err }
-
+func WriteDialogPost(tx *sql.Tx, dialog_id tgtypes.DialogID, msg_id tgtypes.MsgID, chat_id tgtypes.ChatID, json string, msg_ts time.Time) error {
 	sql := "INSERT INTO dialog_posts (dialog_id, msg_id, chat_id, dialog_data, msg_ts) VALUES ($1, $2, $3, $4, $5) ON CONFLICT ON CONSTRAINT dialog_posts_pkey DO UPDATE SET dialog_data = EXCLUDED.dialog_data"
 
 	_, err := tx.Exec(sql, dialog_id, msg_id, chat_id, json, msg_ts)
-
-	settings.Transaction.commit = mine && (err == nil)
 	return err
 }
 
-func EraseDialogPost(settings UpdaterSettings, msg_id tgtypes.MsgID, chat_id tgtypes.ChatID) error {
-	mine, tx := settings.Transaction.PopulateIfEmpty(Db_pool)
-	defer settings.Transaction.Finalize(mine)
-	if settings.Transaction.err != nil { return settings.Transaction.err }
-
+func EraseDialogPost(tx *sql.Tx, msg_id tgtypes.MsgID, chat_id tgtypes.ChatID) error {
 	sql := "DELETE FROM dialog_posts WHERE msg_id = $1 AND chat_id = $2"
 
 	_, err := tx.Exec(sql, msg_id, chat_id)
-
-	settings.Transaction.commit = mine && (err == nil)
 	return err
 }
 
-func FetchDialogPost(settings UpdaterSettings, msg_id tgtypes.MsgID, chat_id tgtypes.ChatID) (*DialogPost, error) {
-	mine, tx := settings.Transaction.PopulateIfEmpty(Db_pool)
-	defer settings.Transaction.Finalize(mine)
-	if settings.Transaction.err != nil { return nil, settings.Transaction.err }
-
+func FetchDialogPost(tx *sql.Tx, msg_id tgtypes.MsgID, chat_id tgtypes.ChatID) (*DialogPost, error) {
 	sql := "SELECT chat_id, msg_id, msg_ts, dialog_id, dialog_data FROM dialog_posts WHERE msg_id = $1 AND chat_id = $2"
 	row := tx.QueryRow(sql, msg_id, chat_id)
 
 	var out DialogPost
 	err := row.Scan(&out.ChatId, &out.MsgId, &out.MsgTs, &out.DialogId, &out.DialogData)
 	if err != nil { return nil, err }
-
-	settings.Transaction.commit = mine
 	return &out, err
 }
