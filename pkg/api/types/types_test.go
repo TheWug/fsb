2c0d5ce6294c844bc00afc5264cb8699994b65ee
Ryan S <wuuug@telegram>
2021-07-29 14:41:00 -0700

    Add a bunch of tests for api/types

--- pkg/api/types/types_test.go
+++ pkg/api/types/types_test.go
@@ -2,8 +2,9 @@ package types
 
 import (
 	"github.com/thewug/fsb/pkg/api/tags"
-	
+
 	"testing"
+	"strings"
 )
 
 func Test_Simple(t *testing.T) {
@@ -11,6 +12,12 @@ func Test_Simple(t *testing.T) {
 	Questionable.String()
 	Safe.String()
 	Original.String()
+
+	TCGeneral.Value()
+	TSONewest.String()
+	ASOStatus.String()
+	ASApproved.String()
+	Upvote.Value()
 }
 
 func Test_RatingFromTagSet(t *testing.T) {
@@ -31,6 +38,104 @@ func Test_RatingFromTagSet(t *testing.T) {
 			out := RatingFromTagSet(v.value)
 			if out != v.rating {
 				t.Errorf("\nExpected: %s\nActual:   %s\n", v.rating, out)
+			}
+		})
+	}
+}
+
+func Test_PageSelector(t *testing.T) {
+	testcases := map[string]struct{
+		value int
+		after, before, page string
+	}{
+		"zero": {0, "a0", "b0", "0"},
+		"normal": {999, "a999", "b999", "999"},
+	}
+
+	for k, v := range testcases {
+		t.Run(k, func(t *testing.T) {
+			ps := After(v.value)
+			if ps.String() != v.after { t.Errorf("Unexpected output (After): got %s, expected %s", ps.String(), v.after) }
+			ps = Before(v.value)
+			if ps.String() != v.before { t.Errorf("Unexpected output (Before): got %s, expected %s", ps.String(), v.before) }
+			ps = Page(v.value)
+			if ps.String() != v.page { t.Errorf("Unexpected output (Page): got %s, expected %s", ps.String(), v.page) }
+		})
+	}
+
+	t.Run("blank", func(t *testing.T) {
+		ps := PageSelector{}
+		if ps.String() != "" { t.Errorf("Unexpected output (Blank PageSelector): got %s, expected %s", ps.String(), "") }
+	})
+}
+
+func Test_PostsAfterChangeSeq(t *testing.T) {
+	testcases := map[string]struct{
+		change int
+		containsEach []string
+	}{
+		"simple": {1000, []string{"status:any", "order:change_asc", "change:>1000"}},
+	}
+
+	for k, v := range testcases {
+		t.Run(k, func(t *testing.T) {
+			result := PostsAfterChangeSeq(v.change)
+			for _, x := range v.containsEach {
+				if !strings.Contains(result, x) { t.Errorf("Unexpected output: result %s does not contain %s", result, x) }
+			}
+		})
+	}
+}
+
+func Test_PostsAfterId(t *testing.T) {
+	testcases := map[string]struct{
+		id int
+		containsEach []string
+	}{
+		"simple": {1000, []string{"status:any", "order:id_asc", "id:>1000"}},
+	}
+
+	for k, v := range testcases {
+		t.Run(k, func(t *testing.T) {
+			result := PostsAfterId(v.id)
+			for _, x := range v.containsEach {
+				if !strings.Contains(result, x) { t.Errorf("Unexpected output: result %s does not contain %s", result, x) }
+			}
+		})
+	}
+}
+
+func Test_DeletedPostsAfterId(t *testing.T) {
+	testcases := map[string]struct{
+		id int
+		containsEach []string
+	}{
+		"simple": {1000, []string{"status:deleted", "order:id_asc", "id:>1000"}},
+	}
+
+	for k, v := range testcases {
+		t.Run(k, func(t *testing.T) {
+			result := DeletedPostsAfterId(v.id)
+			for _, x := range v.containsEach {
+				if !strings.Contains(result, x) { t.Errorf("Unexpected output: result %s does not contain %s", result, x) }
+			}
+		})
+	}
+}
+
+func Test_SinglePostByMd5(t *testing.T) {
+	testcases := map[string]struct{
+		md5 string
+		containsEach []string
+	}{
+		"simple": {"DEADBEEF", []string{"md5:DEADBEEF"}},
+	}
+
+	for k, v := range testcases {
+		t.Run(k, func(t *testing.T) {
+			result := SinglePostByMd5(v.md5)
+			for _, x := range v.containsEach {
+				if !strings.Contains(result, x) { t.Errorf("Unexpected output: result %s does not contain %s", result, x) }
 			}
 		})
 	}
