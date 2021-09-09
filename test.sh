c13ba3c4c43f701a8358366dfe60b9fa0fe04a31
Ryan S <wuuug@telegram>
2021-09-09 02:15:21 -0700

    Delete old build scripts

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-go test ./... github.com/thewug/{dml,reqtify} -coverprofile cover.out && 
-(
-	go tool cover -func=cover.out
-	go tool cover -html=cover.out -o cover.html &&
-		ln -fs `readlink -f cover.html` /storage/wug/samba/scratch/cover.html
-)
